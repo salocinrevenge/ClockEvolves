d76c263ab050aa816c84cfab1e71a5be99a931c4
Nícolas Hecker Silva <nicolashecker.nhs@gmail.com>
2024-12-04 13:33:52 -0300

    ajustado tamanho da engrenagem e tamanho da viga

--- src/engrenagem.py
+++ src/engrenagem.py
@@ -3,16 +3,25 @@ from poligono import Poligono
 from algebra import rotaciona
 
 class Engrenagem(Poligono):
-    def __init__(self, pos: tuple, raio = 32, dentes = None, massa = 1, space = None, elasticity = 0.3, friction = 0.0, color = None, angulo = 0, categoria = 1, escala = 1) -> None:
+    def __init__(self, pos: tuple, raio = 64, dentes = None, massa = 1, space = None, elasticity = 0.6, friction = 0.0, color = None, angulo = 0, categoria = 1, escala = 1, tamanho_dente = 10) -> None:
         raio = int(raio*escala)
-        if raio < 16:
-            raise ValueError("raio de engrenagem deve ser no mínimo 16")
-        if dentes is None:
-            dentes = int(raio/4)
-        pontos = self.get_points(raio, dentes, angulo)
-        super().__init__(pontos, pos = pos, massa = massa, elasticity = elasticity, friction = friction, color = color, space = space, categoria = categoria, meta_info = {"tipo":"engrenagem"}, escala=escala)
 
-    def get_points(self, raio, dentes, angulo):
+        circunferencia = 2*np.pi*raio
+        ndentes = int(circunferencia//(tamanho_dente*2)) # *2 para considerar o espaço entre os dentes
+        
+        if ndentes < 4:
+            raise ValueError("engrenagem deve ter no mínimo 4 dentes, tamanho muito pequeno")
+
+        novo_raio = ndentes*tamanho_dente/np.pi
+
+        if dentes is None:
+            print(f"Dentes não especificados, criando engrenagem com {ndentes} dentes e raio {novo_raio}")
+            dentes = ndentes
+            raio = novo_raio
+        pontos = self.get_points(raio, dentes, angulo, tamanho_dente*1.5)
+        super().__init__(pontos, pos = pos, massa = massa, elasticity = elasticity, friction = friction, color = color, space = space, categoria = categoria, meta_info = {"tipo":"engrenagem"}, escala=1)
+
+    def get_points(self, raio, dentes, angulo, altura_dente):
         # um poligono com 20 pontos é equivalente a uma circunferencia
         pontos = []
         lados = dentes*5
@@ -20,12 +29,12 @@ class Engrenagem(Poligono):
 
         for i in range(lados):
             angulo = 2*np.pi*i/lados
-            mutiplier = 1
+            acrecimo = 0
             if i % 5 == 0 or i % 5 == 1:
             # if i % 3 == 0:
-                mutiplier = 1.5
-            x = float(raio*mutiplier*np.cos(angulo))
-            y = float(raio*mutiplier*np.sin(angulo))
+                acrecimo = altura_dente
+            x = float((raio+acrecimo)*np.cos(angulo))
+            y = float((raio+acrecimo)*np.sin(angulo))
             pontos.append((x,y))
         pontos = np.array(pontos)
         pontos = pontos @ rotaciona(angulo)
--- src/viga.py
+++ src/viga.py
@@ -2,7 +2,7 @@ import numpy as np
 from poligono import Poligono
 
 class Viga(Poligono):
-    def __init__(self, pos: np.ndarray, comprimento = 100, largura = 10, massa = 1, elasticity = 0.3, friction = 1.0, space = None, color = None, categoria = 1, escala=1) -> None: # largura padrao é 10
+    def __init__(self, pos: np.ndarray, comprimento = 200, largura = 10, massa = 1, elasticity = 0.3, friction = 1.0, space = None, color = None, categoria = 1, escala=1) -> None: # largura padrao é 10
         self.pontos = np.array([[-comprimento//2,-largura//2],[comprimento//2,-largura//2],[comprimento//2,largura//2],[-comprimento//2,largura//2]])
         self.comprimento = comprimento
         self.largura = largura
